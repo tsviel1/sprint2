d33b60fb616358c3f29c749bcf322e83baea84f9
Tsviel Sillam <tsviel15@gmail.com>
2022-06-15 16:04:00 +0300

    2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+'use strict'
+
+var gGalleryLength = 18
+
 function onInit() {
-    
+    renderGallery()
+}
+
+function renderGallery() {
+    var strHTML = '';
+	for (var i = 1; i <= gGalleryLength; i++) {
+        `<div class="card flex">
+            <img src="img/meme-imgs (square)/${i}.jpg" alt="">
+        </div>`
+		}
+	
+	var elGallery = document.querySelector('main');
+	elGallery.innerHTML = strHTML;
 }
